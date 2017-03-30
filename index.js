3d1c6f9dcb48787ae92eead73933b26bf7c5258b
Victor Powell <vp@uber.com>
2017-03-29 18:59:57 -0700

    Fix comment syntax typo.

--- index.js
+++ index.js
@@ -1,81 +1,77 @@
 // @flow
 
-/*::
+/*:: export type Position = [number, number]; */
 
-export type Position = [number, number];
-
-export type Point = {|
+/*:: export type Point = {|
   type: 'Point',
   coordinates: Position,
   bbox?: Array<number>,
   crs?: { type: string, properties: mixed }
-|};
+|}; */
 
-:: export type MultiPoint = {|
+/*:: export type MultiPoint = {|
   type: 'MultiPoint',
   coordinates: Array<Position>,
   bbox?: Array<number>,
   crs?: { type: string, properties: mixed }
-|};
+|}; */
 
-export type LineString = {|
+/*:: export type LineString = {|
   type: 'LineString',
   coordinates: Array<Position>,
   bbox?: Array<number>,
   crs?: { type: string, properties: mixed }
-|};
+|}; */
 
-export type MultiLineString = {|
+/*:: export type MultiLineString = {|
   type: 'MultiLineString',
   coordinates: Array<Array<Position>>,
   bbox?: Array<number>,
   crs?: { type: string, properties: mixed }
-|};
+|}; */
 
-export type Polygon = {|
+/*:: export type Polygon = {|
   type: 'Polygon',
   coordinates: Array<Position>,
   bbox?: Array<number>,
   crs?: { type: string, properties: mixed }
-|};
+|}; */
 
-export type MultiPolygon = {|
+/*:: export type MultiPolygon = {|
   type: 'MultiPolygon',
   coordinates: Array<Array<Position>>,
   bbox?: Array<number>,
   crs?: { type: string, properties: mixed }
-|};
+|};  */
 
-export type GeometryTypes =
+/*:: export type GeometryTypes =
   | 'Point'
   | 'MultiPoint'
   | 'LineString'
   | 'MultiLineString'
   | 'Polygon'
   | 'MultiPolygon'
-  | 'GeometryCollection';
+  | 'GeometryCollection'; */
 
-export type Geometry = Point | MultiPoint | LineString | MultiLineString | Polygon | MultiPolygon;
+/*:: export type Geometry = Point | MultiPoint | LineString | MultiLineString | Polygon | MultiPolygon; */
 
-export type GeometryCollection = {|
+/*:: export type GeometryCollection = {|
   type: 'GeometryCollection',
   geometries: Array<Geometry>,
   bbox?: Array<number>,
   crs?: { type: string, properties: mixed }
-|};
+|}; */
 
-export type Feature<T: Geometry> = {|
+/*:: export type Feature<T: Geometry> = {|
   type: 'Feature',
   geometry: T,
   properties: ?{ [key: string]: ?mixed },
   bbox?: Array<number>,
   crs?: { type: string, properties: mixed }
-|};
+|}; */
 
-export type FeatureCollection = {|
+/*:: export type FeatureCollection = {|
   features: Array<Feature<Geometry>>,
   bbox?: Array<number>,
   crs?: { type: string, properties: mixed }
-|};
-
-*/
+|}; */
